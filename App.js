232ea52be87738764b77dc12f53f207a70baf7c0
Fedora-Eugenio <35465318+Fedora-Eugenio@users.noreply.github.com>
2018-06-23 14:35:17 -0400

    Paquetes de navegacion

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>DomiNation</Text>
+        <Button
+        title="INICIO" 
+        onPress={() =>
+          Alert.alert("HE PULSADO EL BOTON")}
+        />
       </View>
     );
   }
