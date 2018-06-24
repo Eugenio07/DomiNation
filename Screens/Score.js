9995ad89b7d5801d66c7af8157625dc07dd886f7
Fedora-Eugenio <35465318+Fedora-Eugenio@users.noreply.github.com>
2018-06-23 22:37:09 -0400

    InputText
    Ya se puede escribir el nombre de los equipos

--- Screens/Score.js
+++ Screens/Score.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image, Button, Alert} from 'react-native';
 import { createStackNavigator,} from 'react-navigation';
-
+import TeamName from './Screens/TeamName'
 
 export default class Score extends React.Component {
   render() {
@@ -9,6 +9,7 @@ export default class Score extends React.Component {
     return (
       <View style={styles.container}>
         <Text>Score</Text>
+
         <Button
         title="Fin" 
         onPress={() =>
